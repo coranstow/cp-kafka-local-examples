9bb339a12ebdf46756c5ca96fef8b8237a646357
coranstow <71357604+coranstow@users.noreply.github.com>
2021-10-11 12:34:31 +1100

    Commented out the install of cfk operator since we want to deploy with Helm

--- Multi-K8s-Stretch/0.prep.sh
+++ Multi-K8s-Stretch/0.prep.sh
@@ -6,7 +6,7 @@ do
   kubectl create namespace confluent-dc$i
   kubectl config set-context --current --namespace=confluent-dc$i
   helm repo update --namespace confluent-dc$i
-  helm upgrade --install confluent-operator confluentinc/confluent-for-kubernetes --namespace confluent-dc$i
+#  helm upgrade --install confluent-operator confluentinc/confluent-for-kubernetes --namespace confluent-dc$i
 done
 
 
