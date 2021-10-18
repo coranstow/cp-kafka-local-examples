d8729b131a67b49bb63d4f16708555f35b00b48b
coranstow <71357604+coranstow@users.noreply.github.com>
2021-10-18 15:09:03 +1100

    Cleaned up files

--- Multi-K8s-Stretch/cluster0/set-aws-cluster.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-aws eks update-kubeconfig --region ap-southeast-2 --name eks-cstow-dc0
-
-#aws eks update-kubeconfig --region ap-southeast-2 --name eks-cluster-dc0 --role-arn arn:aws:iam::492737776546:role/ConfluentSAAdminRole
--- Multi-K8s-Stretch/cluster1/set-aws-cluster.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-aws eks update-kubeconfig --region ap-northeast-2 --name eks-cstow-dc1
